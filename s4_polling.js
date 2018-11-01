432c6c21e0816f337ed9f7a86cee106707a6d9e4
Mitchell Pontague <mitchell.pontague@gmail.com>
2018-11-01 10:17:16 +0800

    add update and compression

---
+++ s4_polling.js
@@ -0,0 +1,45 @@
+var request = require('request');
+var cams_polling = require('./cams_polling');
+var db = require('./database');
+
+setInterval(poll, 30000);
+poll();
+
+async function poll() {
+  var rows =
+      await db
+          .query(
+              'SELECT * FROM guardians g JOIN guardian_types gt ON g.type = gt.index WHERE nickname LIKE "s4"')
+          .then(rows => {
+            return rows;
+          })
+          .catch(err => {
+            console.log('error: ' + err);
+          });
+
+  rows.forEach(element => {
+    request.get('http://' + element.ip + '/api/monitor', function(err, res, body) {
+      if (err) {
+        console.log('error: ' + err);
+      } else if (res && res.statusCode) {
+        body = JSON.parse(body);
+        if (body.series4) {
+          db.query(
+                'INSERT INTO s4_data VALUES ((SELECT name FROM guardians WHERE ip = ?), now())',
+                [
+                  element.ip
+                ])
+              .then(res => {
+                console.log(res);
+              })
+              .catch(err => {
+                console.log('error: ' + err);
+              });
+        }
+        if (body.cams) {
+          cams_polling(db, body.cams);
+        }
+      }
+    });
+  });
+}

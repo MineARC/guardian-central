f3df7da52471083534bdfc71d267ebea948d8065
Mitchell Pontague <mitchell.pontague@gmail.com>
2019-04-11 10:41:24 +0800

    Use native then followup clean

--- elv_polling.js
+++ elv_polling.js
@@ -8,13 +8,16 @@ poll();
 
 function poll() {
   db.getConnection()
-      .then(conn => conn.query('SELECT * FROM guardians g JOIN guardian_types gt ON g.type = gt.index WHERE nickname LIKE "elv"'))
-      .then(rows => { rows.forEach(element => { single(element); }); })
-      .catch(console.log)
-      .finally(() => {
-        if (conn)
-          conn.end();
-      });
+      .then(conn => {
+        conn.query('SELECT * FROM guardians g JOIN guardian_types gt ON g.type = gt.index WHERE nickname LIKE "elv"')
+            .then(rows => { rows.forEach(element => { single(element); }); })
+            .catch(console.log)
+            .then(() => {
+              if (conn)
+                conn.end();
+            });
+      })
+      .catch(console.log);
 }
 
 function single(element) {
@@ -32,15 +35,17 @@ function single(element) {
       if (validJSON) {
         if (body.elv) {
           db.getConnection()
-              .then(conn => conn.query(
-                        'INSERT INTO elv_data (guardian, time, data) VALUES ((SELECT name FROM guardians WHERE ip = ?), now(), ?)',
-                        [ element.ip, body.elv ]))
-              .then(res => { console.log(res); })
-              .catch(console.log)
-              .finally(() => {
-                if (conn)
-                  conn.end();
-              });
+              .then(conn => {
+                conn.query('INSERT INTO elv_data (guardian, time, data) VALUES ((SELECT name FROM guardians WHERE ip = ?), now(), ?)',
+                           [ element.ip, body.elv ])
+                    .then(console.log)
+                    .catch(console.log)
+                    .then(() => {
+                      if (conn)
+                        conn.end();
+                    });
+              })
+              .catch(console.log);
         }
         if (body.cams) {
           cams_polling(db, element.ip, body.cams);

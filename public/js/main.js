e433f20b1a5d1ef464eefe9f6720eb2430c75c35
Mitchell Pontague <mitchell.pontague@gmail.com>
2019-04-16 09:15:58 +0800

    Update alert script

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,23 @@
-$(document).ready(function ($) {
+$(document).ready(function($) {
   setInterval(updateFleetAndAlerts, 10000);
   updateFleetAndAlerts();
 
   function updateFleetAndAlerts() {
-    $.get('/api/hosts/').then(function (data) {
+    $.get('/api/hosts/').then(function(data) {
       var fleethtml = '';
       var alertshtml = '';
-      data.forEach(function (host) {
-        fleethtml += '<li><a href="/chamber/' + host.hostname + '"><i class="icon-tag"></i><span>' + (host.alias ? host.alias : host.hostname.split('-')[1]) + '</span></a></li>';
+      for (var host in data) {
+        fleethtml += '<li><a href="/chamber/' + host.hostname + '"><i class="icon-tag"></i><span>' +
+                     (host.alias ? host.alias : host.hostname.split('-')[1]) + '</span></a></li>';
         for (var types in host.alarms_active) {
-          host.alarms_active[types].forEach(function (alert) {
-            alertshtml += '<li class="list-group-item"><div class="clear"><i class="fa fa-exclamation-circle m-r-xs"></i><span class="alef">' + alert + '</span></div><small class="text-muted">' + (host.alias ? host.alias : host.hostname.split('-')[1]) + '</small></li>';
-          }, this);
+          for (var alert in host.alarms_active[types]) {
+            alertshtml +=
+                '<li class="list-group-item"><div class="clear"><i class="fa fa-exclamation-circle m-r-xs"></i><span class="alef">' +
+                alert + '</span></div><small class="text-muted">' + (host.alias ? host.alias : host.hostname.split('-')[1]) +
+                '</small></li>';
+          }
         }
-      }, this);
+      }
       $('#fleet').html(fleethtml);
       $('#alerts').html(alertshtml);
     });
